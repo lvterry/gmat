42d4e470705f4b0e3236a14e43354302829a62bf
Terry Wang <lvterry@gmail.com>
2016-10-12 22:39:27 +0800

    add pagination for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,9 @@ class PostsController < ApplicationController
     @tags = Tag.all
     @category = params[:category]
     if @category == "" || @category.nil?
-      @posts = Post.order(top: :desc, created_at: :desc)
+      @posts = Post.order(top: :desc, created_at: :desc).page(params[:page]).per_page(10)
     else
-      @posts = Post.where("tags LIKE '%#{@category}%'").order(top: :desc, created_at: :desc)
+      @posts = Post.where("tags LIKE '%#{@category}%'").order(top: :desc, created_at: :desc).page(params[:page]).per_page(10)
     end
   end
 
