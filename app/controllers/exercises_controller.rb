d4ee11feeba184805e3fa07255f9ea718f07dc61
Terry Wang <lvterry@gmail.com>
2017-04-30 22:59:30 +0800

    display filters based on dynamic data

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,6 @@
 class ExercisesController < ApplicationController
+  before_action :set_labels
+
   def index
     @exercises = Exercise.all
     @count = Exercise.count
@@ -6,5 +8,11 @@ class ExercisesController < ApplicationController
 
   def show
     @exercise = Exercise.find params[:id]
+  end
+
+  def set_labels
+    @difficulties = ['500-600', '600-700', '700-750', '750+']
+    @subjects = ['语法SC', '逻辑CR', '阅读RC', '数学PS', '数学DS', '作文AWA', '推理IR']
+    @books = ['OG12', 'OG15', 'OG16', 'OG17', 'Prep07', 'Manhattan']
   end
 end
