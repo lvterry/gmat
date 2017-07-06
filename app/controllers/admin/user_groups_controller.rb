fd2813c6d3f5280b210dbb5d28f4c4c8e29542d9
Terry Wang <lvterry@gmail.com>
2017-07-06 22:33:23 +0800

    validate user group name presence

--- app/controllers/admin/user_groups_controller.rb
+++ app/controllers/admin/user_groups_controller.rb
@@ -6,33 +6,31 @@ class Admin::UserGroupsController < AdminController
 
   def new
     @user_group = UserGroup.new
-    @difficulties = Label.where(category: '难度')
-    @subjects = Label.where(category: '题型')
-    @books = Label.where(category: '来源')
   end
 
   def create
     @user_group = UserGroup.new(user_group_params)
     if @user_group.save
       flash[:notice] = "保存成功"
+      redirect_to edit_admin_user_group_path(@user_group)
     else
-      flash[:notice] = '保存失败'
+      render "admin/user_groups/new"
     end
-    redirect_to edit_admin_user_group_path(@user_group)
+    
   end
 
   def edit
     @user_group = UserGroup.find params[:id]
-    @difficulties = Label.where(category: '难度')
-    @subjects = Label.where(category: '题型')
-    @books = Label.where(category: '来源')
   end
 
   def update
-    @group = UserGroup.find params[:id]
-    @group.update(user_group_params)
-    flash[:notice] = '保存成功'
-    redirect_to edit_admin_user_group_path(@group)
+    @user_group = UserGroup.find params[:id]
+    if @user_group.update(user_group_params)
+      flash[:notice] = '保存成功'
+      redirect_to edit_admin_user_group_path(@user_group)
+    else
+      render "admin/user_groups/edit"
+    end
   end
 
   def add_users
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,6 +2,10 @@ class UserGroup < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :labels
 
+  validate do |group|
+    group.errors[:base] << "请填写组名"
+  end
+
   def label_names
     self.labels.map do |label|
       label.name
