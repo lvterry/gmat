f6a845e83d0afa9b6985b70afbc2ad1f8f30e0e8
Terry Wang <lvterry@gmail.com>
2017-07-06 23:44:09 +0800

    add filters to admin exercise list

--- app/controllers/admin/exercises_controller.rb
+++ app/controllers/admin/exercises_controller.rb
@@ -1,7 +1,20 @@
 class Admin::ExercisesController < AdminController
 
   def index
-    @exercises = Exercise.order(created_at: :desc).page(params[:page]).per_page(20)
+    if filter_is_empty?(params[:filters])
+      @exercises = Exercise.order(created_at: :desc).page(params[:page]).per_page(20)
+      @filters = [0,0,0]
+    else
+      labels = labels_for_search params[:filters]
+      search = Exercise.search do
+        #fulltext params[:query]
+        with(:label_ids).all_of(labels)
+        paginate page: params[:page] || 1, per_page: 20
+      end
+
+      @exercises = search.results
+      @filters = params[:filters]
+    end
   end
 
   def new
@@ -41,6 +54,14 @@ class Admin::ExercisesController < AdminController
     params.require(:exercise).permit(:title, :math_guide, {:choices => []}, :anwser,
       {:guides => []}, :video_url, :exercise_tags,
       {:label_ids => []}, :exclusive)
+  end
+
+  def filter_is_empty?(filters)
+    return filters.nil? || (filters.join("") == "000")
+  end
+
+  def labels_for_search(filters)
+    return filters.map { |item| item.to_i if item != "0" }.compact
   end
 
 end
