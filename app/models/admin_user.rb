7919c984fb9bddfa097a003373f9850dbedef178
Terry Wang <lvterry@gmail.com>
2017-07-11 00:09:36 +0800

    setup admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,7 @@
 class AdminUser < ApplicationRecord
-    validates :name, :password, presence: true
+    validates :password, presence: true
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+    before_save { self.name = name.downcase }
     has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    
+  end
 end
