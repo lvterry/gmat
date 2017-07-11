b5169b1222591d48f6f21c6eafcbfd9c345749f4
Terry Wang <lvterry@gmail.com>
2017-07-11 23:07:30 +0800

    add login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,3 +1,21 @@
 class Admin::SessionsController < AdminController
   layout 'simple'
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def create
+    user = AdminUser.find_by(name: params[:session][:name].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:admin_user_id] = user.id
+      redirect_to '/admin'
+    else
+      flash[:danger] = '用户名密码错误'
+      render 'new'
+    end
+  end
 end
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,10 @@
 class AdminController < ActionController::Base
   protect_from_forgery with: :exception
   layout 'admin'
+
+  def current_user
+    @current_user ||= AdminUser.find(session[:admin_user_id]) if session[:admin_user_id]
+  end
+
+  helper_method :current_user
 end
