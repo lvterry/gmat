10412dd275c97de6e4d67b8fb4b67c0917a53553
Terry Wang <lvterry@gmail.com>
2017-08-17 22:31:03 +0800

    force user to add mobile number

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,5 @@
 class Label < ApplicationRecord
   has_and_belongs_to_many :exercises
-  #has_and_belongs_to_many :user_groups
   has_many :permissions
   has_many :user_groups, through: :permissions
 
