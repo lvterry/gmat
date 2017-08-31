c237439b2285b196873b3f4b46440adcecdcecfd
Terry Wang <lvterry@gmail.com>
2017-08-31 18:35:41 +0800

    fix user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    # # only allow one session for one account
-    # if session[:user_id]
-    #   user = User.find session[:user_id]
-    #   if session.id == user.session_id
-    #     @current_user = user
-    #   else
-    #     reset_session
-    #   end
-    # end
-    @current_user = User.find 4
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # only allow one session for one account
+    if session[:user_id]
+      user = User.find session[:user_id]
+      if session.id == user.session_id
+        @current_user = user
+      else
+        reset_session
+      end
+    end
+    # @current_user = User.find 4
   end
 
   helper_method :current_user
