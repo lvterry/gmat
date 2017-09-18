3dadbdb3b4dd6c9cdae7df72e0185f1909c1fda2
Terry Wang <lvterry@gmail.com>
2017-09-18 10:33:52 +0800

    add pinyin column when adding users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
       unionid: auth_hash.info.unionid,
       provider: auth_hash.provider,
       token: auth_hash.credentials.token
+      pinyin: Pinyin.t(auth_hash.nickname).gsub(' ','')
     )
     user
   end
