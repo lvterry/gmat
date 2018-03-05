3ea94ec55250332c1590b6b7d64f9e3300cc2239
Terry Wang <lvterry@gmail.com>
2018-03-05 23:36:04 +0800

    add a label to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   belongs_to :teacher
 
   def self.subjects
-    ['数学','逻辑','语法','阅读','机经','线下']
+    ['数学','逻辑','语法','阅读','机经','线下', '入门']
   end
 
   def subject_name
