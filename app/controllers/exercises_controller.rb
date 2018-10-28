633fc6d8fbe2664d55fd30ee7b37625d315fbbd5
Terry Wang <lvterry@gmail.com>
2018-10-28 22:30:16 +0800

    disable solr search from front-end

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -32,17 +32,31 @@ class ExercisesController < ApplicationController
     @labels = params[:labels].split(",") if params[:labels].present?
     @exclusive = params[:exclusive]
 
-    search = Exercise.search do
-      fulltext params[:query]
-      with(:hide_from_view, false)
-      with(:exclusive, true) if params[:exclusive].present?
-      with(:label_ids).all_of(params[:labels].split(",")) if params[:labels].present?
-      order_by(:seq)
-      paginate page: params[:page] || 1, per_page: 10
+    # disable solr search temproraily because it doesnt work
+    # search = Exercise.search do
+    #   fulltext params[:query]
+    #   with(:hide_from_view, false)
+    #   with(:exclusive, true) if params[:exclusive].present?
+    #   with(:label_ids).all_of(params[:labels].split(",")) if params[:labels].present?
+    #   order_by(:seq)
+    #   paginate page: params[:page] || 1, per_page: 10
+    # end
+    #
+    # @exercises = search.results
+    # @total = search.total
+    
+    unless @labels.nil?
+      integer_labels = @labels.map{|label| label.to_i }
+      ex = Exercise.viewable.reject{ |e| (e.label_ids & integer_labels).empty? }
+      @exercises = ex.paginate(page: params[:page], per_page: 10)
+      @total = ex.size
+    else
+      @exercises = Exercise.viewable.page(params[:page]).per_page(10)
+      @total = @exercises.count
     end
+    
+    
 
-    @exercises = search.results
-    @total = search.total
     render :index
   end
 
