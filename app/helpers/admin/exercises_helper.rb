43ba926379fac9eb5855769395d127eacc725106
Terry Wang <lvterry@gmail.com>
2018-12-10 22:38:56 +0800

    show user avatar in admin

--- app/helpers/admin/exercises_helper.rb
+++ app/helpers/admin/exercises_helper.rb
@@ -2,4 +2,8 @@ module Admin::ExercisesHelper
   def make_options(labels)
     labels.map { |label| [label.name, label.id] }
   end
+  
+  def wechat_avatar_url(url)
+    return url.nil? ? "" : url
+  end
 end
